8f4013296700c42faa15a3d9f90559d976c1d91a
m1nh101 <minhdd148@gmail.com>
2024-09-08 23:33:25 +0700

    fix error

---
+++ DATN_ACV_DEV/VoucherConversation.cs
@@ -0,0 +1,16 @@
+﻿using DATN_ACV_DEV.Entity;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace DATN_ACV_DEV
+{
+    public class VoucherConversation : ValueConverter<VoucherUnit, string>
+    {
+        public VoucherConversation()
+            : base(
+                 v => v.ToString(),
+                 d => (VoucherUnit)Enum.Parse(typeof(VoucherUnit), d)
+            )
+        {
+        }
+    }
+}
