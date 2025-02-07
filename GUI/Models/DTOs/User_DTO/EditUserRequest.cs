8045df86fb081ed3b40fa07b49814afcd9b5ed77
twanenh <anhemnha8x@gmail.com>
2025-02-07 21:58:00 +0700

    update

--- GUI/Models/DTOs/User_DTO/EditUserRequest.cs
+++ GUI/Models/DTOs/User_DTO/EditUserRequest.cs
@@ -6,11 +6,14 @@ namespace GUI.Model_DTO.User_DTO
     public class EditUserRequest : BaseRequest
     {
         public Guid ID { get; set; }
+        [Required(ErrorMessage = "Chưa nhập tên đăng nhập")]
+        [RegularExpression(@"\S+", ErrorMessage = "Tên đăng nhập không được chứa khoảng trắng")]
         public string UserName { get; set; }
         [EmailAddress(ErrorMessage = "Email không hợp lệ")]
         [Required(ErrorMessage = "Chưa nhập email")]
         public string Email { get; set; }
         public string? Position { get; set; }
+        [Required(ErrorMessage = "Chưa nhập mã nhân viên")]
         public string UserCode { get; set; }
         [Required(ErrorMessage = "Chưa nhập tên")]
         public string FullName { get; set; }
