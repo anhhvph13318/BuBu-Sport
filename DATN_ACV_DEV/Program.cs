92dece270ed12c9ba9f4d9f27e19c83e6b69ad22
abcdefopquan <nguyenquan14214@gmail.com>
2025-04-06 19:22:34 +0700

    cap nhat orderdetail

--- DATN_ACV_DEV/Program.cs
+++ DATN_ACV_DEV/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DBContext>(options => options.UseSqlServer("Data Source=TW4NENH\\TUANANH;Initial Catalog=DB_BuBu_04_04;Integrated Security=True;Trust Server Certificate=True; Encrypt=False;"));
+builder.Services.AddDbContext<DBContext>(options => options.UseSqlServer("Data Source=DEC\\SQLEXPRESS;Initial Catalog=DB_BuBu_06_04;Integrated Security=True;Trust Server Certificate=True; Encrypt=False;"));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
