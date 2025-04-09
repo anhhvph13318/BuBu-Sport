7360994a5d29caaa796925633d4dc39d48afd19a
anhhvph13318 <73866644+anhhvph13318@users.noreply.github.com>
2025-04-10 03:40:05 +0700

    order offline order draft

---
+++ GUI/Models/DTOs/Order_DTO/UpdateItemOrderRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace GUI.Models.DTOs.Order_DTO
+{
+    public class UpdateItemOrderRequest
+    {
+        public IList<OrderItem> Items { get; set; }
+        public int Status { get; set; }
+    }
+}
