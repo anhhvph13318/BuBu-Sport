90fa33c7f441d0542902c36a4f9153679fb65fc1
twanenh <anhemnha8x@gmail.com>
2025-04-10 21:59:43 +0700

    hotfix

--- GUI/Controllers/OrderController.cs
+++ GUI/Controllers/OrderController.cs
@@ -242,12 +242,6 @@ public class OrderController : Controller
 
         if (rawResponse.IsSuccessStatusCode)
         {
-            if (rawResponse.RequestMessage.Method == HttpMethod.Patch)
-            {
-                TempData["SuccessMessage"] = "Cập nhật hóa đơn thành công !!!";
-                return Redirect("http://localhost:5011/orders/create/instore");
-            }
-
             var orders = await FetchOrderList();
 
             try
