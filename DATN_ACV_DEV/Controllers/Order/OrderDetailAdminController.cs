fd0a39f5ac6fef29fc3a8168822ebb533a48714e
twanenh <anhemnha8x@gmail.com>
2025-05-03 23:03:19 +0700

    fix orderdetail discount

--- DATN_ACV_DEV/Controllers/Order/OrderDetailAdminController.cs
+++ DATN_ACV_DEV/Controllers/Order/OrderDetailAdminController.cs
@@ -94,7 +94,7 @@ public class OrderDetailAdminController : ControllerBase
                 Id = orderDetail.ProductId,
                 Code = _context.TbProducts.Where(c => c.Id ==
                 _context.TbProductDetails.Where(a => a.Id == orderDetail.ProductId).Select(a => a.ProductId).FirstOrDefault()).Select(c => c.Code).FirstOrDefault(),
-                Price = _context.TbProductDetails.Where(c => c.Id == orderDetail.ProductId).Select(c => c.Price).FirstOrDefault(),
+                Price = orderDetail.Price,
                 Quantity = orderDetail.Quantity,
                 ProductImage = _context.TbImages.Where(c => c.Id ==
                 _context.TbProductDetails.Where(a => a.Id == orderDetail.ProductId).Select(a => a.ImageId).FirstOrDefault()).Select(c => c.Url).FirstOrDefault(),
@@ -192,7 +192,7 @@ public class OrderDetailAdminController : ControllerBase
                 Items = e.TbOrderDetails.Select(d => new OrderItem()
                 {
                     Id = d.ProductId,
-                    Price = d.Product.Price,
+                    Price = d.Price,
                     Quantity = d.Quantity,
                     ProductImage = d.Product.tb_Image.Url,
                     ProductName = d.Product.Name
@@ -277,7 +277,8 @@ public class OrderDetailAdminController : ControllerBase
                     Items = e.TbOrderDetails.Select(d => new OrderItem()
                     {
                         Id = _context.TbOrderDetails.Where(c => c.Id == d.Id).Select(c => c.ProductId).FirstOrDefault(),
-                        Price = _context.TbProductDetails.Where(c => c.Id == _context.TbOrderDetails.Where(c => c.Id == d.Id).Select(c => c.ProductId).FirstOrDefault()).Select(c => c.Price).FirstOrDefault(),
+                        //Price = _context.TbProductDetails.Where(c => c.Id == _context.TbOrderDetails.Where(c => c.Id == d.Id).Select(c => c.ProductId).FirstOrDefault()).Select(c => c.Price).FirstOrDefault(),
+                        Price = d.Price,
                         Quantity = d.Quantity,
                         ProductImage = _context.TbImages.Where(c => c.Id == _context.TbProductDetails.Where(a => a.Id == d.ProductId).Select(c => c.ImageId).FirstOrDefault()).Select(c => c.Url).FirstOrDefault(),
                         ProductName = _context.TbProducts.Where(v=>v.Id == _context.TbProductDetails.Where(c => c.Id == 
