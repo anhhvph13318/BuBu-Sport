a095e2eb5d141a0b1e87b1b68d0dcce815d8e9dd
anhhvph13318 <73866644+anhhvph13318@users.noreply.github.com>
2025-05-05 11:08:16 +0700

    update code 11_08

--- DATN_ACV_DEV/Controllers/Order/AdminCreateOrderController.cs
+++ DATN_ACV_DEV/Controllers/Order/AdminCreateOrderController.cs
@@ -132,6 +132,9 @@ namespace DATN_ACV_DEV.Controllers.Order
                 }
                 else
                 {
+                    TbProductDetail tbProductDetail = new TbProductDetail();
+                    tbProductDetail = _context.TbProductDetails.Where(c => c.Id == Guid.Parse(item.Id)).FirstOrDefault();
+                    tbProductDetail.Quantity -= item.Quantity;
                     product.Quantity -= item.Quantity;
                 }
             }
